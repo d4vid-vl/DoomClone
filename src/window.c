9e06ea6777a99bd318609cd739402119ea48f171
David <157435386+d4vid-vl@users.noreply.github.com>
2024-12-25 02:08:46 -0300

    Added player functionality (not visible)

--- src/window.c
+++ src/window.c
@@ -1,5 +1,9 @@
 #include "window.h"
+#include <math.h>
+#include <stdio.h>
 
+// Initialize window
+// * param window: Window struct
 void windowInit(Window* window) {
   window->height = WIN_HEIGHT;
   window->width = WIN_WIDTH;
@@ -20,24 +24,68 @@ void windowInit(Window* window) {
   }
 }
 
+// Destroy window
+// * param window: Window struct
 void windowDestroy(Window* window) {
   SDL_DestroyRenderer(window->renderer);
   SDL_DestroyWindow(window->window);
   SDL_Quit();
 }
 
-void windowEvents(Window* window) {
+// Handle window events
+// * param window: Window struct
+void windowEvents(Window* window, Player* player) {
   SDL_Event event;
+  int dx = sin(player->angle*M_PI/180)*10.0;
+  int dy = cos(player->angle*M_PI/180)*10.0;
   while (SDL_PollEvent(&event)) {
     switch (event.type) {
       case SDL_QUIT:
         window->isRunning = false;
         break;
-      case SDL_KEYDOWN:
+      case SDL_KEYDOWN: 
         if (event.key.keysym.sym == SDLK_ESCAPE) {
           window->isRunning = false;
+        } else if (event.key.keysym.sym == SDLK_w) {
+          player->x += dx;
+          player->y += dy;
+        } else if (event.key.keysym.sym == SDLK_a) {
+          player->angle -= 4;
+          if (player->angle < 0) {
+            player->angle += 360;
+          }
+        } else if (event.key.keysym.sym == SDLK_s) {
+          player->x -= dx;
+          player->y -= dy;
+        } else if (event.key.keysym.sym == SDLK_d) {
+          player->angle += 4;
+          if (player->angle >= 360) {
+            player->angle -= 360;
+          }
+        } else if (event.key.keysym.sym == SDLK_UP) {
+          player->z -= 4;
+        } else if (event.key.keysym.sym == SDLK_DOWN) {
+          player->z += 4;
+        } else if (event.key.keysym.sym == SDLK_LEFT) {
+          player->look -= 1;
+        } else if (event.key.keysym.sym == SDLK_RIGHT) {
+          player->look += 1;
         }
         break;
     }
   }
+}
+
+// Render everything on screen
+// * param window: Window struct
+void renderShow(Window* window) {
+    // Draw canvas
+    SDL_SetRenderDrawColor(window->renderer, 0, 0, 0, 255);
+    SDL_RenderClear(window->renderer);
+    
+    // Draw player
+    SDL_SetRenderDrawColor(window->renderer, 255, 255, 255, 255);
+    SDL_Rect player = {window->width/2 - 5, window->height/2 - 5, 10, 10};
+    SDL_RenderFillRect(window->renderer, &player);
+    SDL_RenderPresent(window->renderer);
 }
