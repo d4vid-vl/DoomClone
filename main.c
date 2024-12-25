18accdb0d90b62669d093c8f589464fb22651d79
David <157435386+d4vid-vl@users.noreply.github.com>
2024-12-25 02:08:56 -0300

    Startup player

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include "window.h"
 
 Window window;
+Player player = {70, -110, 20, 0, 0};
 
 int main(void)
 {    
     windowInit(&window);
     while (window.isRunning) {
-        windowEvents(&window);
+       renderShow(&window);
+       windowEvents(&window, &player); 
     }
     windowDestroy(&window);
     return 0;
